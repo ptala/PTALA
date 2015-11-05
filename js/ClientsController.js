0b70744878943208920c9571753f11a73b8cd8a5
Pranesha Bunsee <pbunsee@gmail.com>
2015-11-05 11:23:14 -0500

    Filter applied to Clients page

--- js/ClientsController.js
+++ js/ClientsController.js
@@ -1,166 +1,227 @@
 angular.module('ptala')
 .controller('ClientsController', function ClientsController($scope, $rootScope,$sce) {
 
+  $scope.filters = { };
 
 	$scope.clients = [
 	{
 		img: "cambria",
 		name:"Cambria",
-		videoid:"126493347"
+		videoid:"126493347",
+                category: "Experiential Activations",
+                sequence: 1
 	},
 	{
 		img: "nordstrom",
 		name:"Nordstrom",
-		videoid:"141869324"
+		videoid:"141869324",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "biogen",
 		name:"Biogen",
-		videoid:"124529350"
+		videoid:"124529350",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "cvs",
 		name:"CVS Health",
-		videoid:"127095096"
+		videoid:"127095096",
+                category: "Experiential Activations",
+                sequence: 1
 	},
 	{
 		img: "qualcomm",
 		name:"Qualcomm",
-		videoid:"138404677"
+		videoid:"138404677",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "prudentialfinancial",
 		name:"Prudential Financial",
-		videoid:"139225422"
+		videoid:"139225422",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "smartcars",
 		name:"Smart Cars",
-		videoid:"140954435"
+		videoid:"140954435",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "dcl",
 		name:"D.C. Lottery",
-		videoid:"86116915"
+		videoid:"86116915",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "tropicana",
 		name:"Tropicana",
-		videoid:"59112441"
+		videoid:"59112441",
+                category: "Experiential Activations",
+                sequence: 1
 	},
 	{
 		img: "philips",
 		name:"Philips",
-		videoid:"58926610"
+		videoid:"58926610",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "ravens.jpeg",
 		name:"Baltimore Ravens",
-		videoid:"59689251"
+		videoid:"59689251",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "todayshow",
 		name:"The Today Show",
-		videoid:"61678699"
+		videoid:"61678699",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "kelloggs",
 		name:"Kelloggs",
-		videoid:"61377575"
+		videoid:"61377575",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "redcross",
 		name:"American Red Cross",
-		videoid:"61467299"
+		videoid:"61467299",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "ncaa",
 		name:"NCAA March Madness Event",
-		videoid:"62256402"
+		videoid:"62256402",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "onedirection",
 		name:"One Direction Pop-Up Store",
-		videoid:"64068286"
+		videoid:"64068286",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "reuters",
 		name:"Thomas Reuters",
-		videoid:"70293547"
+		videoid:"70293547",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "redbull",
 		name:"Redbull",
-		videoid:"95929278"
+		videoid:"95929278",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "nintendo",
 		name:"Nintendo Wii U Pop-Up",
-		videoid:"72462204"
+		videoid:"72462204",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "lv",
 		name:"Louis Vuiton",
-		videoid:"73313897"
+		videoid:"73313897",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "puma",
 		name:"PUMA",
-		videoid:"88586782"
+		videoid:"88586782",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "amazon",
 		name:"Amazon",
-		videoid:"79524654"
+		videoid:"79524654",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "NFL",
 		name:"NFL Kickoff",
-		videoid:"74156380"
+		videoid:"74156380",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "oceanspray",
 		name:"Ocean Spray Cranberry Event",
-		videoid:"76912907"
+		videoid:"76912907",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "popsugar",
 		name:"Pop Sugar",
-		videoid:"96035818"
+		videoid:"96035818",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "celebrity",
 		name:"Celebrity Cruises",
-		videoid:"85039127"
+		videoid:"85039127",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "qatar",
 		name:"Qatar",
-		videoid:"87403972"
+		videoid:"87403972",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "nest",
 		name:"Nest",
-		videoid:"88535108"
+		videoid:"88535108",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 
 	{
 		img: "nespresso",
 		name:"Nespresso",
-		videoid:"88539916"
+		videoid:"88539916",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	},
 	{
 		img: "tazo",
 		name:"Tazo Iced Tea Event",
-		videoid:"100357105"
+		videoid:"100357105",
+                category: "OOH / Billboard Campaigns",
+                sequence: 1
 	}
 	]
 
