85cbfe258b0052e01e5ea04e5273fb6288b813e8
Pranesha Bunsee <pbunsee@gmail.com>
2015-11-09 14:44:19 -0500

    Clients grouped by Category

--- js/ClientsController.js
+++ js/ClientsController.js
@@ -1,183 +1,266 @@
 angular.module('ptala')
 .controller('ClientsController', function ClientsController($scope, $rootScope,$sce) {
 
+  function sortOn( collection, name ) {
+    collection.sort(
+      function( a, b ) {
+       if ( a[ name ] <= b[ name ] ) {
+         return( -1 );
+       }
+       return( 1 );
+      }
+    );
+  };
 
-	$scope.clients = [
-	{
-		img: "cambria",
-		name:"Cambria",
-		videoid:"126493347"
-	},
-	{
-		img: "nordstrom",
-		name:"Nordstrom",
-		videoid:"141869324"
-	},
-	{
-		img: "biogen",
-		name:"Biogen",
-		videoid:"124529350"
-	},
-	{
-		img: "cvs",
-		name:"CVS Health",
-		videoid:"127095096"
-	},
-	{
-		img: "qualcomm",
-		name:"Qualcomm",
-		videoid:"138404677"
-	},
-	{
-		img: "prudentialfinancial",
-		name:"Prudential Financial",
-		videoid:"139225422"
-	},
-	{
-		img: "smartcars",
-		name:"Smart Cars",
-		videoid:"140954435"
-	},
-	{
-		img: "dcl",
-		name:"D.C. Lottery",
-		videoid:"86116915"
-	},
-	{
-		img: "tropicana",
-		name:"Tropicana",
-		videoid:"59112441"
-	},
-	{
-		img: "philips",
-		name:"Philips",
-		videoid:"58926610"
-	},
-	{
-		img: "ravens.jpeg",
-		name:"Baltimore Ravens",
-		videoid:"59689251"
-	},
-	{
-		img: "todayshow",
-		name:"The Today Show",
-		videoid:"61678699"
-	},
-	{
-		img: "kelloggs",
-		name:"Kelloggs",
-		videoid:"61377575"
-	},
-	{
-		img: "redcross",
-		name:"American Red Cross",
-		videoid:"61467299"
-	},
-	{
-		img: "ncaa",
-		name:"NCAA March Madness Event",
-		videoid:"62256402"
-	},
-	{
-		img: "onedirection",
-		name:"One Direction Pop-Up Store",
-		videoid:"64068286"
-	},
-	{
-		img: "reuters",
-		name:"Thomas Reuters",
-		videoid:"70293547"
-	},
-	{
-		img: "redbull",
-		name:"Redbull",
-		videoid:"95929278"
-	},
-	{
-		img: "nintendo",
-		name:"Nintendo Wii U Pop-Up",
-		videoid:"72462204"
-	},
-	{
-		img: "lv",
-		name:"Louis Vuiton",
-		videoid:"73313897"
-	},
+  $scope.groupBy = function( attribute ) {
+    $scope.groups = [];
+    sortOn( $scope.clients, attribute );
+    var groupValue = "_INVALID_GROUP_VALUE_";
+    for ( var i = 0 ; i < $scope.clients.length ; i++ ) {
+      var client = $scope.clients[ i ];
+      if ( client[ attribute ] !== groupValue ) {
+        var group = {
+                      label: client[ attribute ],
+                      clients: []
+                    };
+        groupValue = group.label;
+        $scope.groups.push( group );
+      }
+      group.clients.push( client );
+    }
+  };
 
-	{
-		img: "puma",
-		name:"PUMA",
-		videoid:"88586782"
-	},
+  $scope.groups = [];
 
-	{
-		img: "amazon",
-		name:"Amazon",
-		videoid:"79524654"
-	},
-
-	{
-		img: "NFL",
-		name:"NFL Kickoff",
-		videoid:"74156380"
-	},
-
-	{
-		img: "oceanspray",
-		name:"Ocean Spray Cranberry Event",
-		videoid:"76912907"
-	},
-
-	{
-		img: "popsugar",
-		name:"Pop Sugar",
-		videoid:"96035818"
-	},
-
-	{
-		img: "celebrity",
-		name:"Celebrity Cruises",
-		videoid:"85039127"
-	},
-
-	{
-		img: "qatar",
-		name:"Qatar",
-		videoid:"87403972"
-	},
-
-	{
-		img: "nest",
-		name:"Nest",
-		videoid:"88535108"
-	},
-
-	{
-		img: "nespresso",
-		name:"Nespresso",
-		videoid:"88539916"
-	},
-	{
-		img: "tazo",
-		name:"Tazo Iced Tea Event",
-		videoid:"100357105"
-	}
-	]
-
-	$scope.openClient = 0;
-	$scope.changeOpenClientTo = function(index) {
-		$('.hide').removeClass('hide');
-		$scope.openClient = index;
-		$("html, body").animate({ scrollTop: 0 + 'px' });
+  $scope.clients = [
+    {
+      img: "cambria",
+      name:"Cambria",
+      videoid:"126493347",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "nordstrom",
+      name:"Nordstrom",
+      videoid:"141869324",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "biogen",
+      name:"Biogen",
+      videoid:"124529350",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "cvs",
+      name:"CVS Health",
+      videoid:"127095096",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "qualcomm",
+      name:"Qualcomm",
+      videoid:"138404677",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "prudentialfinancial",
+      name:"Prudential Financial",
+      videoid:"139225422",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "smartcars",
+      name:"Smart Cars",
+      videoid:"140954435",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "dcl",
+      name:"D.C. Lottery",
+      videoid:"86116915",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "tropicana",
+      name:"Tropicana",
+      videoid:"59112441",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "philips",
+      name:"Philips",
+      videoid:"58926610",
+      category:"Exp",
+      sequence:1
+    },
+    {
+      img: "ravens.jpeg",
+      name:"Baltimore Ravens",
+      videoid:"59689251",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "todayshow",
+      name:"The Today Show",
+      videoid:"61678699",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "kelloggs",
+      name:"Kelloggs",
+      videoid:"61377575",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "redcross",
+      name:"American Red Cross",
+      videoid:"61467299",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "ncaa",
+      name:"NCAA March Madness Event",
+      videoid:"62256402",
+      category:"Exp",
+      sequence:1
+    },
+    {
+      img: "onedirection",
+      name:"One Direction Pop-Up Store",
+      videoid:"64068286",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "reuters",
+      name:"Thomas Reuters",
+      videoid:"70293547",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "redbull",
+      name:"Redbull",
+      videoid:"95929278",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "nintendo",
+      name:"Nintendo Wii U Pop-Up",
+      videoid:"72462204",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "lv",
+      name:"Louis Vuiton",
+      videoid:"73313897",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "puma",
+      name:"PUMA",
+      videoid:"88586782",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "amazon",
+      name:"Amazon",
+      videoid:"79524654",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "NFL",
+      name:"NFL Kickoff",
+      videoid:"74156380",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "oceanspray",
+      name:"Ocean Spray Cranberry Event",
+      videoid:"76912907",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "popsugar",
+      name:"Pop Sugar",
+      videoid:"96035818",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "celebrity",
+      name:"Celebrity Cruises",
+      videoid:"85039127",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "qatar",
+      name:"Qatar",
+      videoid:"87403972",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "nest",
+      name:"Nest",
+      videoid:"88535108",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "nespresso",
+      name:"Nespresso",
+      videoid:"88539916",
+      category:"OOH",
+      sequence:1
+    },
+    {
+      img: "tazo",
+      name:"Tazo Iced Tea Event",
+      videoid:"100357105",
+      category:"OOH",
+      sequence:1
+    }
+  ]
+  
+  $scope.openClient = 0;
+  $scope.changeOpenClientTo = function(index) {
+    $('.hide').removeClass('hide');
+    $scope.openClient = index;
+    $("html, body").animate({ scrollTop: 0 + 'px' });
+  };
 	
-		
-	};
-	
-	$scope.getVimeoSource = function() {
-		return	$sce.trustAsResourceUrl("http://player.vimeo.com/video/" + $scope.clients[$scope.openClient].videoid  + "?title=0&amp;byline=0&amp;portrait=0");
-	};
+  $scope.getVimeoSource = function() {
+    return $sce.trustAsResourceUrl("http://player.vimeo.com/video/" + 
+                                   $scope.clients[$scope.openClient].videoid  + 
+                                   "?title=0&amp;byline=0&amp;portrait=0");
+    };
 
-	$scope.openClients = function() {
-		$("html, body").animate({ scrollTop: $('.logos').position().top + 'px' });
-	};
+  $scope.openClients = function() {
+    $("html, body").animate({ scrollTop: $('.logos').position().top + 'px' });
+  };
+
 });
+
