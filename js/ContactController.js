2b87621547822463625537196a6b60338b7aa0bd
Pranesha Bunsee <pbunsee@gmail.com>
2015-11-12 21:29:13 -0500

    Refined location marker on map

--- js/ContactController.js
+++ js/ContactController.js
@@ -1,26 +1,29 @@
 angular.module('ptala')
 .controller('ContactController', function ContactController($scope, $rootScope) {
+
   function initialize() {
     var mapCanvas = document.getElementById('map');
-    var myLatlng = new google.maps.LatLng(40.7638482,-73.9719923);
+    var myLatlng = new google.maps.LatLng(40.763724,-73.970887);
     var mapOptions = {
-      //center: new google.maps.LatLng(44.5403, -78.5463),
       center: myLatlng,
       zoom: 18,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions)
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
-        animation: google.maps.Animation.DROP,
-        title: 'PTALA'
+        label: 'PTALA',
+        title: 'PTALA',
+        optimized: false,
+        animation: google.maps.Animation.DROP
     });
     marker.setMap(map);
-  }
+    //marker.setValues({ map: map, label: "PTALA" });
+  };
+
   angular.element(document).ready(function () {
-    //$('.mapImage').css({left:(($(window).width() - $('.mapImage').width())/2) + 'px'});
     google.maps.event.addDomListener(window, 'load', initialize);
   });
+
 });
